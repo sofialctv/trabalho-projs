6e96865f65ffdba5f53a85c0ab10391f1a5864f6
Sofia de Alcantara <sofiaalcantaras@outlook.com>
2025-01-24 08:39:00 -0300

    removendo verificação de 'if (app.Environment.IsDevelopment())'

--- colabAPI/Program.cs
+++ colabAPI/Program.cs
@@ -51,22 +51,11 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "coLAB API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
-
 app.Use(async (context, next) =>
 {
     context.Response.Headers.Append("Referrer-Policy", "no-referrer-when-downgrade");
     await next();
 });
-
 
 // Habilita CORS
 app.UseCors("AllowAllOrigins");
